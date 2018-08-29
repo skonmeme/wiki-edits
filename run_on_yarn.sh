65e76af64c14f23ca49168d441ddeb40811a0b46
Sung Gon Yi <skonmeme@gmail.com>
2018-08-29 18:59:19 +0900

    run on Hadoop Yarn

---
+++ run_on_yarn.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+export HADOOP_CLASSPATH=`hadoop classpath`
+
+jar=$(ls target/wiki-edits*.jar | sort -V | tail -n 1)
+
+ENTRYCLASS="com.skt.skon.wikiedits.WikipediaAnalysis"
+
+PARAM="--channel-list en,ko,jp,de,es,fr,ru,pt,it,zh,pl \
+--session-gap 60 \
+--brokers 192.168.10.254:9092 \
+--topic-summary wiki-edits-summary --topic-contents wiki-edits-contents \
+--group-id other \
+--checkpoint-data-uri hdfs://skonuniverse:8020/flink \
+--checkpoint-state-backend fs \
+--checkpoint-interval $((5*1000*60))"
+
+flink run \
+  --jobmanager yarn-cluster \
+  --yarndetached \
+  --yarnqueue default \
+  --yarncontainer 2 \
+  --yarnjobManagerMemory 1024 \
+  --yarntaskManagerMemory 1024 \
+  --yarnslots 2 \
+  --class $ENTRYCLASS \
+  $jar \
+  $PARAM
